b7a864b03847ccc2165900c116d424ed7cc361a2
g-larose <larose.g@fromtier.com>
2024-03-04 18:07:18 -0500

    worked on GiveawayCommands

--- GiveawayBot/Commands/GiveawayCommands.cs
+++ GiveawayBot/Commands/GiveawayCommands.cs
@@ -1,4 +1,6 @@
-﻿using Guilded.Commands;
+﻿using Guilded.Base;
+using Guilded.Commands;
+using Guilded.Permissions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,45 @@ namespace GiveawayBot.Commands
 {
     public class GiveawayCommands: CommandModule
     {
+        [Command(Aliases = [ "giveaway", "ga" ])]
+        [Description("giveaway command")]
+        public async Task Giveaway(CommandEvent invokator, string command = "")
+        {
+            var authorId = invokator.Message.CreatedBy;
+            var serverId = invokator.Message.ServerId;
+            var author = await invokator.ParentClient.GetMemberAsync((HashId)serverId!, authorId);
 
+            if (command is null || command.Equals(""))
+            {
+                await invokator.ReplyAsync($"{author.Name} I was expecting a command but found []. example commands are [ \"start\", \"stop\", \"pause\" ]");
+            }
+            else
+            {
+                var permissions = await invokator.ParentClient.GetMemberPermissionsAsync((HashId)serverId!, authorId);
+                if (!permissions.Contains(Permission.ManageServer))
+                {
+                    await invokator.ReplyAsync($"{author.Name} it seems you **do not** have the correct permissions to run this command, command **ignored**");
+                }
+                else
+                {
+                    switch (command)
+                    {
+                        case "start":
+                            await invokator.ReplyAsync($"{author.Name} you used [start] command");
+                            break;
+                        case "stop":
+                             await invokator.ReplyAsync($"{author.Name} you used [stop] command");
+                            break;
+                        case "pause":
+                             await invokator.ReplyAsync($"{author.Name} you used [pause] command");
+                            break;
+                        default:
+                             await invokator.ReplyAsync($"{author.Name} sorry, I don't recognise that command");
+                            break;
+
+                    }
+                }
+            }
+        }
     }
 }
