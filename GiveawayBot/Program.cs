0294524120c5334aebc2d1f6476c69790912548c
g-larose <larose.g@fromtier.com>
2024-03-04 14:57:23 -0500

    Add project files.

---
+++ GiveawayBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GiveawayBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
